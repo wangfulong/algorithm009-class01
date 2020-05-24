cd9335b93a78b1cfed2e5d73b985c27180d890f4
wangfulong <wangfulong@yiqixie.com>
2020-05-24 16:31:52 +0800

    update 42 641

---
+++ Week_01/42.接雨水.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ * Two pointers
+ */
+var trap = function(height) {
+  if (height == null || height.length == 0) return 0;
+  let l = 0;
+  let r = height.length - 1;
+  let lMax = 0;
+  let rMax = 0;
+
+  let res = 0;
+  while (l < r) {
+    lMax = Math.max(lMax, height[l]);
+    if (height[l] < lMax) {
+      res += lMax - height[l];
+    }
+
+    rMax = Math.max(rMax, height[r]);
+    if (height[r] < rMax) {
+      res += rMax - height[r];
+    }
+    height[l] < height[r] ? l++ : r--;
+  }
+  return res;
+};
+// @lc code=end
+
